12c320faad3885afb9f43d4a3d47b7108c559471
chrishannon <chrishannon21@gmail.com>
2024-07-10 18:24:29 +0100

    added scan, load, close wav file functions to audio mgr

--- src/AudioFileManager.cpp
+++ src/AudioFileManager.cpp
@@ -1,15 +1,15 @@
-// TODO: make a .h file once this class is somehwat done
-
 #include <stdio.h>
 #include "daisy_pod.h"
-// #include "daisysp.h"
 #include "AudioFileManager.h"
 #include "ff.h"
 
 using namespace daisy;
 
-FatFSInterface fsi;
-SdmmcHandler sd;
+// should i init these here or in main class? 
+// here for now, move to main if i need to use them in other classes
+// FatFSInterface fsi;
+// SdmmcHandler sd;
+
 // functions i need
 
 // init function that takes sd card path, mounts  returns bool for successs?
@@ -17,12 +17,12 @@ SdmmcHandler sd;
 bool AudioFileManager::Init(){
   SdmmcHandler::Config sd_cfg;
   sd_cfg.Defaults();
-  if (sd.Init(sd_cfg) != SdmmcHandler::Result::OK) {
+  if (sd_->Init(sd_cfg) != SdmmcHandler::Result::OK) {
     return false;
   }
 
-  fsi.Init(FatFSInterface::Config::MEDIA_SD);
-  if (f_mount(&fsi.GetSDFileSystem(),"/",1) != FR_OK){
+  fsi_->Init(FatFSInterface::Config::MEDIA_SD);
+  if (f_mount(&fsi_->GetSDFileSystem(),"/",1) != FR_OK){
     // TODO: return enums here + above
     // for sd error blink function to print to log? 
     return false;
@@ -32,29 +32,89 @@ bool AudioFileManager::Init(){
 }
 
 bool AudioFileManager::ScanWavFiles(){
-  // TODO: change to char arary
-  // std::vector<std::string> wav_files;
   DIR dir;
   FILINFO fno;
-  char name[256];
-  int count=0;
+  /* NOTE:  all file names must be less than 128 chars 
+    TODO: ask if this is fine or if i should actually
+          implement length checks + adding null terminator
+          (atm check is much easier but not as safe)
+  */
+  char name[MAX_FNAME_LEN];
+
+  /* clear names array before use */
+  memset(names_, '\0', sizeof(names_));
+
   // do i need this? if just using "/"
-  const char* path = fsi.GetSDPath();
+  const char* path = fsi_->GetSDPath();
 
   if (f_opendir(&dir,path) != FR_OK){
-    
+    // log error message here
+    return false;
   }
 
+  int count=0;
+  while (f_readdir(&dir, &fno) == FR_OK && fno.fname[0]!=0){
+    if (!(fno.fattrib & (AM_HID|AM_DIR))){
+      /* NOTE: below, we ignore AppleDouble metadata files starting with '._'
+      so if a legit file starts with this prefix, it will be wrongly skipped
+      TODO: fix how this is handled (or just stipulate filenames must be correctly formatted) */
+      if (strncmp(fno.fname, "._",2)==0) continue;
 
+      strcpy(name, fno.fname);      
+      if (strstr(name, ".wav") || strstr(name, ".WAV")){
+        strcpy(names_[count],name);
+        // strncpy(wavnames_[count],name, MAX_FNAME_LEN-1);
+        count++;
+      }
+    } else {
+      // handle directories/hidden files
+      if (fno.fattrib & AM_DIR){
+        pod_->seed.PrintLine("found dir: %s", fno.fname);
+        continue;
+      } else if (fno.fattrib & AM_HID){
+        pod_->seed.PrintLine("found hidden file: %s", fno.fname);
+        continue;
+      }
+    }
+  }
+  f_closedir(&dir);
 
+  // NOTE: converted to 1 index rather than 0 index
+  file_count_ = count+1;
+
+  for (int i=0; i<MAX_FILES; i++){
+    /* stop if filename empty ie no more files */
+    /* NOTE: this could poss break sth so double check it works */
+    if (names_[i][0]!='\0'){
+      break;
+    }
+    pod_->seed.PrintLine(names_[i]);
+  }
+  pod_->seed.PrintLine("final count %d files", count);
+  return true;
 }
 
 
-// scan wav files fn from main file - returns list of filenames
+/* TODO: atm i'm loading by index as this is easier than using the names.
+        however i'm still saving names for (if/)when i get a screen ?
+*/
+bool AudioFileManager::LoadFile(int sel_idx) {
+  if (sel_idx >= file_count_) return false;
 
-// load a file from sd card by filename/pointer to name - return bool
+  pod_->seed.Print("loading file: ");
+  pod_->seed.PrintLine(names_[sel_idx]);
 
-// close file 
+  if (sel_idx != curr_idx_) {
+    f_close(&curr_file_);
+
+  }
+  // NOTE: double check this
+  return f_open(&curr_file_, names_[sel_idx], (FA_OPEN_EXISTING | FA_READ)) == FR_OK;
+}
+
+bool AudioFileManager::CloseFile(){
+  return f_close(&curr_file_) == FR_OK;
+}
 
 // getters for wav file info
 
--- src/AudioFileManager.h
+++ src/AudioFileManager.h
@@ -1,9 +1,13 @@
 class AudioFileManager {
   public:
-    AudioFileManager();
+    AudioFileManager(SdmmcHandler* sd, FatFSInterface* fsi, DaisyPod* pod):
+                      sd_(sd), fsi_(fsi), pod_(pod){};
     bool Init();
-    // TODO: return list
-    bool AudioFileManager::ScanWavFiles();
+    bool ScanWavFiles();
+    bool LoadFile(int file_idx);
+    bool CloseFile();
+
+
     // scan wav files fn from main file - returns list of filenames
 
     // load a file from sd card by filename/pointer to name - return bool
@@ -14,14 +18,29 @@ class AudioFileManager {
 
     // getter for pointer to audio buffer that file is loaded into
     // getter for buffer size?
+
+    static const int MAX_FILES = 32;                      
+    static const int MAX_FNAME_LEN = 128;
+    static const int MAX_BUFFER_SIZE = 32 * 1024 * 1024;
   private:
-    SdmmcHandler sd_;
-    FatFSInterface fsi_;
+    SdmmcHandler* sd_;
+    FatFSInterface* fsi_;
+    DaisyPod* pod_;
+    // list of filenames for logging/screen
+    char names_ [MAX_FILES][MAX_FNAME_LEN];
+    // index of currently selected file
+    int curr_idx_;
+    // currently selected file
+    FIL curr_file_; 
+    // total no of files scanned
+    // NOTE: 1 indexed not 0 indexed - do i need to do this?
+
+    int file_count_;
+    // 32MB audio buffer
+    float DSY_SDRAM_BSS aubuffer_[MAX_BUFFER_SIZE];
+
 
   // wav file info header struct
-  // list of wav file names
   // file index
-  // current file
-  // audio buffer? 
   // playback status? 
 };
